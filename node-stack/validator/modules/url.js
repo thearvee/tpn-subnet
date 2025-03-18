99c8187e929e168b02f56e127a26fdfd52e84b11
Mentor <mentor@palokaj.co>
2025-03-18 17:12:28 +0100

    Fix url handling in env

--- node-stack/validator/modules/url.js
+++ node-stack/validator/modules/url.js
@@ -5,11 +5,14 @@ const { PUBLIC_URL, PUBLIC_VALIDATOR_URL, PUBLIC_PORT } = process.env
 // Base url based on environment
 let base_url = PUBLIC_VALIDATOR_URL || PUBLIC_URL
 
+// Remove trailing slash
+base_url = base_url.replace( /\/$/, '' )
+
 // Check if public url has a port
-const has_port = `${ PUBLIC_VALIDATOR_URL }`.includes( ':' )
+const has_port = `${ base_url }`.includes( ':' )
 
-if( has_port && PUBLIC_PORT ) log.error( `You specified a PUBLIC_PORT=${ PUBLIC_PORT } but your PUBLIC_URL=${ PUBLIC_VALIDATOR_URL } also has a port specified, this will break!` )
+if( has_port && PUBLIC_PORT ) log.error( `You specified a PUBLIC_PORT=${ PUBLIC_PORT } but your base url ${ PUBLIC_VALIDATOR_URL } also has a port specified, this will break!` )
 
-if( PUBLIC_PORT ) base_url = `${ PUBLIC_VALIDATOR_URL }:${ PUBLIC_PORT }`
+if( PUBLIC_PORT ) base_url = `${ base_url }:${ PUBLIC_PORT }`
 
 export { base_url }
