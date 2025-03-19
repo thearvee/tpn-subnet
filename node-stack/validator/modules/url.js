de7478b9602962d10e62194e92a0ff874f2cd92b
Mentor <mentor@palokaj.co>
2025-03-19 15:20:58 +0100

    WIP

--- node-stack/validator/modules/url.js
+++ node-stack/validator/modules/url.js
@@ -15,10 +15,13 @@ if( CI_MODE ) {
 base_url = `${ base_url }`.replace( /\/$/, '' )
 
 // Check if public url has a port
-const has_port = `${ base_url }`.includes( ':' )
+const has_port = `${ base_url }`.match( /:\d*$/ )
 
 if( has_port && PUBLIC_PORT ) log.error( `You specified a PUBLIC_PORT=${ PUBLIC_PORT } but your base url ${ PUBLIC_VALIDATOR_URL } also has a port specified, this will break!` )
 
-if( PUBLIC_PORT ) base_url = `${ base_url }:${ PUBLIC_PORT }`
+if( PUBLIC_PORT && !base_url.includes( `:${ PUBLIC_PORT }` ) ) {
+    log.info( `Adding port ${ PUBLIC_PORT } to base url` )
+    base_url = `${ base_url }:${ PUBLIC_PORT }`
+}
 
 export { base_url }
