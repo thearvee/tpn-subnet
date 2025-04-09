18a83019ecd9db5ef010e6674ce7d1ef1a4adc89
Mentor <mentor@palokaj.co>
2025-04-09 14:33:26 +0200

    🐛 remove grace window because this must be done neuron level

--- node-stack/validator/modules/scoring.js
+++ node-stack/validator/modules/scoring.js
@@ -42,7 +42,7 @@ export async function score_request_uniqueness( request, disable_rate_limit=fals
 
     // TEMPOTARY rate limit until neuron code filters duplicate ips
     const last_seen = cache( `last_seen_${ unspoofable_ip }` )
-    const cooldown_minutes = .5
+    const cooldown_minutes = .1
     const minutes_since_seen = ( Date.now() - last_seen ) / 1000 / 60
     if( !disable_rate_limit && last_seen && minutes_since_seen < cooldown_minutes ) {
         log.info( `Request from ${ unspoofable_ip } seen ${ minutes_since_seen } minutes ago, scoring as 0` )
