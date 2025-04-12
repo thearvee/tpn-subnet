8b679168a346968621348bdfb5a918f5d05ea286
Mentor <mentor@palokaj.co>
2025-04-12 22:44:32 +0200

    Namespaced attempt

--- node-stack/validator/modules/wireguard.js
+++ node-stack/validator/modules/wireguard.js
@@ -316,7 +316,7 @@ export async function validate_wireguard_config( { peer_config, peer_id } ) {
 
 
     // Command to test connectivity via WireGuard.
-    const curl_command = `curl -m ${ test_timeout_seconds } --interface ${ interface_id } -s ${ challenge_url }`
+    const curl_command = `ip netns exec ${ interface_id } curl -m ${ test_timeout_seconds } -s ${ challenge_url }`
 
     // Cleanup commands for the namespace and interfaces.
     const cleanup_command = `
