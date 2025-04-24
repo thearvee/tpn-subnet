d8a31784b680261d13381797eb7030f31047d2e4
ronx-labs <ron.sam624@gmail.com>
2025-04-24 08:51:43 -0400

    fix: add check is_serving when choosing miners

--- sybil/utils/uids.py
+++ sybil/utils/uids.py
@@ -16,8 +16,8 @@ def check_uid_availability(
         bool: True if uid is available, False otherwise
     """
     # # Filter non serving axons.
-    # if not metagraph.axons[uid].is_serving:
-    #     return False
+    if not metagraph.axons[uid].is_serving:
+        return False
     # # Filter validator permit > 1024 stake.
     # if metagraph.validator_permit[uid]:
     #     if metagraph.S[uid] > vpermit_tao_limit:
