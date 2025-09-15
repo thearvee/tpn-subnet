31a873e4da9b3c7d1d9d96b99bc3fbbb4acfeda2
Mentor <mentor@palokaj.co>
2025-09-15 14:26:59 +0200

    💩 wip

--- federated-container/routes/validator/score.js
+++ federated-container/routes/validator/score.js
@@ -2,6 +2,7 @@ import { Router } from "express"
 import { score_mining_pools } from "../../modules/scoring/score_mining_pools.js"
 import { cache, log } from "mentie"
 import { get_pool_scores } from "../../modules/database/mining_pools.js"
+import { request_is_local } from "../../modules/networking/network.js"
 
 
 export const router = Router()
@@ -25,10 +26,14 @@ router.get( "/force", async ( req, res ) => {
 
 router.get( '/mining_pools', async ( req, res ) => {
 
+    // Exit if not local request
+    if( !request_is_local( req ) ) return res.status( 403 ).json( { error: `This endpoint may only be called from localhost` } )
+    log.info( `Received request for mining pool scores` )
+
     try { 
         // Check for cached value
-        const cached_scores = cache( 'mining_pool_scores' ) || {} 
-        if( cached_scores ) return cached_scores
+        const cached_scores = cache( 'mining_pool_scores' )
+        if( cached_scores ) return res.json( cached_scores )
 
         // Get updated scores
         const { success, message, scores } = await get_pool_scores()
