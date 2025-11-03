084a10f3687697a05218aceacb9a1501a35cd041
Mentor <mentor@palokaj.co>
2025-11-03 11:41:50 +0100

    🐛 Only run cleanup on relevant tables for a machine

--- federated-container/modules/database/cleanup.js
+++ federated-container/modules/database/cleanup.js
@@ -1,5 +1,6 @@
 import { log } from "mentie"
 import { get_pg_pool } from "./postgres.js"
+import { run_mode } from "../validations.js"
 
 export async function database_cleanup() {
 
@@ -8,15 +9,31 @@ export async function database_cleanup() {
     const month_in_mins = 43800
     const epoch_minutes = 90
 
+    // Define tables to be cleaned up
+    const { mode, validator_mode, miner_mode, worker_mode } = run_mode()
+
     // Table staleness definitions
-    const STALENESS_THRESHOLDS = [
+    const shared_tables = [
+        { table: 'timestamps', time_field: 'updated', max_stale_minutes: year_in_mins }
+    ]
+    const worker_tables = [
+        { table: 'worker_wireguard_configs', time_field: 'expires_at', max_stale_minutes: epoch_minutes },
+    ]
+    const neuron_tables = [
         { table: 'workers', time_field: 'updated_at', max_stale_minutes: epoch_minutes },
         { table: 'worker_performance', time_field: 'updated_at', max_stale_minutes: month_in_mins },
         { table: 'worker_broadcast_metadata', time_field: 'updated', max_stale_minutes: epoch_minutes },
         { table: 'challenge_solution', time_field: 'updated', max_stale_minutes: epoch_minutes },
-        { table: 'scores', time_field: 'updated', max_stale_minutes: epoch_minutes },
-        { table: 'worker_wireguard_configs', time_field: 'expires_at', max_stale_minutes: epoch_minutes },
-        { table: 'timestamps', time_field: 'updated', max_stale_minutes: year_in_mins }
+    ]
+    const validator_tables = [
+        { table: 'mining_pool_metadata_broadcast', time_field: 'updated', max_stale_minutes: epoch_minutes },
+        { table: 'scores', time_field: 'updated', max_stale_minutes: epoch_minutes }
+    ]
+    const STALENESS_THRESHOLDS = [
+        ...shared_tables,
+        ...worker_mode ? worker_tables : [] ,
+        ...miner_mode || validator_mode ? neuron_tables : [] ,
+        ...validator_mode ? validator_tables : [] 
     ]
 
     try {
@@ -31,7 +48,7 @@ export async function database_cleanup() {
 
             // Calculate threshold time
             const threshold_time = Date.now() -  max_stale_minutes * 60_000 
-            log.info( `Cleaning up table ${ table }, removing entries older than ${ max_stale_minutes } minutes` )
+            log.info( `Cleaning up table ${ table } based on field ${ time_field }, removing entries older than ${ max_stale_minutes } minutes` )
             if( isNaN( threshold_time ) ) {
                 log.error( `Invalid threshold time for table ${ table }, skipping cleanup` )
                 continue
@@ -47,7 +64,7 @@ export async function database_cleanup() {
                 log.info( `Deleted ${ delete_result.rowCount } stale entries from table ${ table }` )
 
             } catch ( e ) {
-                log.error( `Error cleaning up table ${ table }:`, e )
+                log.error( `Error cleaning up table ${ table } on ${ mode }:`, e )
             }
 
         }
